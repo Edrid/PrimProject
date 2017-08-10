23f33320a2af65f87781e8403907b664f27ab2f2
Edoardo <ewgrappolini@gmail.com>
2017-08-10 17:13:17 +0200

    General commit

---
+++ Modules+prototypes/BrightManipulation-v2/cmake-build-debug/CMakeFiles/BrightManipulation_v2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BrightManipulation_v2.dir/main.cpp.o"
+  "BrightManipulation_v2.pdb"
+  "BrightManipulation_v2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BrightManipulation_v2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
