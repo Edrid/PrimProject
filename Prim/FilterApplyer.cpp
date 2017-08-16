96196f8d0248be6ca016dee829c1613a067a21a1
Edoardo <ewgrappolini@gmail.com>
2017-08-16 14:52:57 +0200

    Made the skeleton for the 'setKernelStrategy()' method

--- Prim/FilterApplyer.cpp
+++ Prim/FilterApplyer.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "FilterApplyer.h"
+
+void FilterApplyer::setKernelStrategy(FilterType filterID) {
+    delete strategy_;
+    if(filterID == FilterType::GaussianBlur){
+        strategy_ = new GaussianBlur();
+    }
+
+    switch(filterID){
+        case FilterType::GaussianBlur:
+            strategy_ = new GaussianBlur(); //TODO to be implemented
+            break;
+        case FilterType::EdgeDetection:
+            strategy_ = new EdgeDetection(); //TODO to be implemented
+        }
+
+    //TODO so on and so forth for all the filters available
+
+}
--- Prim/FilterApplyer.h
+++ Prim/FilterApplyer.h
@@ -13,28 +13,24 @@ using namespace std;
 typedef vector<vector<int>>* QuantumPointer; //Here I use 'raw' pointers for ease of the implementation. Later maybe change them with smart pointers, but after all 'Premature optimization is the root of all evil'
 
 //TODO some filters need normalizzation, others don't
-enum class filterType{GaussianBlur, EdgeDetection, Sharpen, BoxBlur};
+enum class FilterType{GaussianBlur, EdgeDetection, Sharpen, BoxBlur};
 
 
 
 class FilterApplyer {
 public:
     FilterApplyer(QuantumPointer reds, QuantumPointer greens, QuantumPointer blues, QuantumPointer alphas) : reds_(reds), greens_(greens), blues_(blues), alphas_(alphas){}
-    FilterApplyer();
-    void applyFilter(); //this method has all the necessary information to elaborate the quantums. TODO make sure that the quantum information is always updated (though they should be.. they're pointers after all)
-    void setFilter();
+    //FilterApplyer();
+    void kernelFilter(); //this method has all the necessary information to elaborate the quantums. TODO make sure that the quantum information is always updated (though they should be.. they're pointers after all)
+    void setKernelStrategy(FilterType filterID);
 private:
     //A questo punto cambio i valori dei vettori e faccio renderizzare l'immagine all'ImageElaborator. TODO prima di chiamare qualunque metodo nell'ImageElaborator, assicurarsi che ci sia una chiamata all'undo manager
-    /*QuantumPointer redsp;
-    QuantumPointer greensp;
-    QuantumPointer bluesp;
-    QuantumPointer alphasp;*/
     QuantumPointer reds_;
     QuantumPointer greens_;
     QuantumPointer blues_;
     QuantumPointer alphas_;
 
-
+    KernelFilter *strategy_; //STRATEGY TODO: implement KernelFilter class
 
 };
 
