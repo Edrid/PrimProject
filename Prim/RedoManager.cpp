95aa5d324a224c3d163f3fd89ad9d8fa88c87c21
Bartimeus92 <tommaso.malossi@gmail.com>
2017-09-16 18:02:56 +0200

    Init

---
+++ Prim/RedoManager.cpp
@@ -0,0 +1,12 @@
+//
+// Created by barti on 20/08/17.
+//
+
+#include "RedoManager.h"
+
+Image* RedoManager::redo() {
+    if (!redoVector.empty()) {
+     redoVector.pop_back();
+     return redoVector.back();
+    }
+} //non so cosa mettere nel caso redoVector sia vuoto...
